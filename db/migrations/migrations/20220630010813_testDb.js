db65bd727376ac01a36b9d2abef10f0e94fe2645
Robert Kulp <pedalinon@gmail.com>
2022-06-30 17:16:25 -0700

    first build out iteration using knex.js to build postgress database

--- db/migrations/migrations/20220630010813_testDb.js
+++ db/migrations/migrations/20220630010813_testDb.js
@@ -12,7 +12,7 @@ exports.up = function(knex) {
         // How to set primary key?
         table.increments('company_id'); 
         table.string('name').notNullable();
-        table.string('primary_contact_id').notNullable().unsigned().references('employee_id').onUpdate('CASCADE').onDelete('CASCADE');
+        table.string('primary_contact_id').notNullable().unsigned().references('employee.employee_id').onUpdate('CASCADE').onDelete('CASCADE');
         table.integer('cost_plus').notNullable().unsigned();
         table.string('street').notNullable();
         table.string('city').notNullable();
@@ -23,12 +23,30 @@ exports.up = function(knex) {
         table.float('annual_revenue');
     })
     .createTable('employee', table => {
-        table.increments('employee_id');
+        table.increments('employee_id')notNullable().unsigned().references('companies.primary_contact_id').onUpdate('CASCADE').onDelete('CASCADE');
         table.string('first_name').notNullable();
         table.string('last_name').notNullable();
         table.string('email')notNullable().unique();
         table.string('phone').checkRegex('[0-9]{10}');
         table.string('password').notNullable();
+        table.string('username');
+        table.string('title');
+    })
+    .createTable('invoice', table => {
+        table.integer('company_id').notNullable().unsigned().references('companies.company_id').onUpdate('CASCADE').onDelete('CASCADE');
+        table.integer('invoice_number').notNullable().unsigned();
+        table.float('balance').notNullable();
+        table.float('amount_paid').notNullable().unsigned();
+        table.datetime('date_time', { precision: 6 }).defaultTo(knex.fn.now(6)).notNullable().unsigned();
+        table.boolean('fullfillment')defaultTo('no', { fullfillment: 'fullfillment_value' }).notNullable();
+        table.string('notes', [1000]).nullable();
+        table.integer('order_id').notNullable().unsigned();
+    })
+    .createTable('order', table => {
+        table.integer('order_id').notNullable().unique().unsigned();
+        table.float('total').notNullable().unsigned();
+        table.float('balance').notNullable();
+        table.float('amount_paid').notNullable().unsigned();
     })
 };
 
@@ -41,4 +59,5 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists('companies');
+    .dropTableIfExists('employee');
 };
