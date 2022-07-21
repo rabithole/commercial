0fac6847ec61be43a1d39d7b18721d128908c790
Robert Kulp <pedalinon@gmail.com>
2022-07-20 18:50:22 -0700

    fixed seeds

--- db/migrations/migrations/20220709022224_OrderLineItems.js
+++ db/migrations/migrations/20220709022224_OrderLineItems.js
@@ -5,7 +5,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('order_line_items', table => {
     table.increments();
-    table.integer('order_id');
+    table.integer('order_id').references('id').inTable('orders').onUpdate('CASCADE').onDelete('CASCADE');
     table.integer('product_id'); // References shopify id
     table.integer('quantity');
     table.float('unit_price');
